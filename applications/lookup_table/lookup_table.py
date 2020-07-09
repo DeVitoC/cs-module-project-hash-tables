9bd61124c5b7d253e7d96a6c4fba566071b2c09a
Christopher DeVito <christopher.devito@protonmail.com>
2020-07-09 03:14:55 -0400

    Implemented slowfun in lookup_table.py

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,5 +1,6 @@
 # Your code here
-
+import random
+import math
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
@@ -9,13 +10,24 @@ def slowfun_too_slow(x, y):
 
     return v
 
+factorials = {}
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
+    v = math.pow(x, y)
+    if v in factorials:
+        v = factorials[v]
+    else:
+        key = v
+        v = math.factorial(v)
+        factorials[key] = v
+    v //= (x + y)
+    v %= 982451653
 
+    return v
 
 
 # Do not modify below this line!
