85a0bcbbc065b6c7d4e159705b982c6ada11739b
Christopher DeVito <christopher.devito@protonmail.com>
2020-07-09 13:43:16 -0400

    Implemented expensive_seq.py.

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,9 +1,17 @@
 # Your code here
 
-
+cache = {}
 def expensive_seq(x, y, z):
     # Your code here
-
+    if x <= 0:
+        return y + z
+    elif (x, y, z) in cache.keys():
+        return cache[(x, y, z)]
+    else:
+        first = expensive_seq(x-1, y+1, z)
+        second = expensive_seq(x-2, y+2, z*2)
+        third = expensive_seq(x-3, y+3, z*3)
+        return first + second + third
 
 
 if __name__ == "__main__":
