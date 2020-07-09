284489a445589cada8ec0a693082fbcd033a4de7
Christopher DeVito <christopher.devito@protonmail.com>
2020-07-09 11:41:03 -0400

    Implemented no_dups

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,7 +1,13 @@
 def no_dups(s):
     # Your code here
-
-
+    words_array = s.split()
+    words_dict = {}
+    for word in words_array:
+        words_dict[word] = 1
+    words_array = list(words_dict.keys())
+    s = " "
+    s = s.join(words_array)
+    return s
 
 if __name__ == "__main__":
     print(no_dups(""))
