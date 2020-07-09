5d09957926842741bae204efa655338bf1344255
Christopher DeVito <christopher.devito@protonmail.com>
2020-07-09 11:34:02 -0400

    implemented word count method

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,7 +1,18 @@
+import string
+
 def word_count(s):
     # Your code here
-
-
+    s = s.translate(str.maketrans('', '', '":;,.-+=/\\|[]{}()*^&'))
+    word_array = s.split()
+    num_words = {}
+    for word in word_array:
+        if word == "":
+            continue
+        word = word.lower()
+        if word not in num_words.keys():
+            num_words[word] = 0
+        num_words[word] += 1
+    return num_words
 
 if __name__ == "__main__":
     print(word_count(""))
